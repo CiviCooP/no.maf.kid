d20f08715b7d8fc2fafdf3d19c14f0c6f536fa27
Erik Hommel <hommel@ee-atwork.nl>
2014-03-13 18:27:10 +0100

    BOS1403431 tested 15 digit kid generation for membership contribution locally

--- kid.php
+++ kid.php
@@ -80,7 +80,7 @@ function kid_civicrm_post($op, $objectName, $objectId, &$objectRef) {
     
     // When various entities are created, generate and store KID number
     if ($op == 'create' and !defined('__BYPASS_HOOK_CIVICRM_POST')) {
-
+        
         switch ($objectName) {
             
             case 'Activity':
@@ -105,20 +105,23 @@ function kid_civicrm_post($op, $objectName, $objectId, &$objectRef) {
                 break;
 
             case 'Contribution':
-                
+
                 // if contribution is part of a contribution recur, generate 15 digit kid number
                 if (isset($objectRef->contribution_recur_id) and !empty($objectRef->contribution_recur_id)) {
                     $contact_id = $objectRef->contact_id;
                     $kid_number = kid_number_generate_15digit($contact_id, $objectId);
-					
-					kid_insert($kid_number, $contact_id, $objectId, $objectName);
+                    kid_insert($kid_number, $contact_id, $objectId, $objectName);
                 }
                 /*
                  * BOS1403431 Contributions that are part of a Memberships with AvtaleGiro need 15 digit KID too\
                  * Erik Hommel (CiviCooP) <erik.hommel@civicoop.org> 12 Mar 2014
                  */
+                $finTypeParams = array(
+                    'name'  =>  "Medlem",
+                    'return'=>  "id"
+                );
                 try {
-                    $membershipFinTypeId = civicrm_api3('FinancialType', 'Getsingle', array('name'=>"Medlem"));
+                    $membershipFinTypeId = civicrm_api3('FinancialType', 'Getvalue', $finTypeParams);
                 } catch (CiviCRM_API3_Exception $e) {
                     CRM_Core_Error::fatal(ts("Could not find a valid financial type for Medlem, 
                         error from API entity FinancialType, action Getsingle is : ".$e->getMessage()));
@@ -147,6 +150,7 @@ function kid_civicrm_post($op, $objectName, $objectId, &$objectRef) {
                 if ($objectRef->financial_type_id == $membershipFinTypeId && $objectRef->payment_instrument_id == $avtaleGiroId) {
                     $contact_id = $objectRef->contact_id;
                     $kid_number = kid_number_generate_15digit($contact_id, $objectId);
+                    kid_insert($kid_number, $contact_id, $objectId, $objectName);
                 }
                 // end BOS1403431
                 break;
